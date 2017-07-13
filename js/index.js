3c07583ce5fb25447652f6c65ef2db997dce1b6d
10162543 <BrunoRito@brunomacbookpro.lan>
2017-07-13 12:46:34 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ var txtFun = ["Cerveja", "chá", ];
 
 function geoFindMe() {
     var output = $("#container");
-    
+
     if (!navigator.geolocation){
     output.append("<p>Geolocation is not supported by your browser</p>");
     return;
@@ -19,58 +19,58 @@ function geoFindMe() {
             latitude  = position.coords.latitude;
             longitude = position.coords.longitude;
             /* output.append('<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>'); */
-            
+
             /** Open Weather data **/
             var api = "http://api.openweathermap.org/data/2.5/forecast?q=Lisbon&mode=xml?&APPID=1fba7c3eaa869008374898c6a606fe3e";
 
             var api2 = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&APPID=1fba7c3eaa869008374898c6a606fe3e&units=metric";
-            
+
             /** Forecast 10 days **/
             var api3 = "http://api.openweathermap.org/data/2.5/forecast/daily?&lat=" + latitude + "&lon=" + longitude + "&APPID=1fba7c3eaa869008374898c6a606fe3e&units=metric&cnt=10";
-            
+
             /** Openweather.org API ID **/
             var api_id = "1fba7c3eaa869008374898c6a606fe3e";
-      
+
             var newArray = [];
 
 
             $.getJSON(api3, function(data){
-                
+
                 $(".location").append("<span>" + data.city.name + "</span>");
-                
+
                 /* Get data from openWeather api */
                 for (var i=0; i < data.list.length; i++) {
 
                     var tempMax = Math.round(data.list[i].temp.max);
                     var tempMin = Math.round(data.list[i].temp.min);
-                    
-                    
+
+
                     /* Date */
                     var d = data.list[i].dt;
                     var date = new Date(d*1000);
                     var dateToString = String(date);
                     var weekDay = dateToString.slice(0, 3);
                     var weekDayLong = dateToString.slice(3, 15);
-                    
+
                     /* Icons */
                     var iconW = data.list[i].weather[0].icon;
                     var iconWnum = parseInt(iconW.slice(0, 2));
-                    
-                    
+
+
                     newArray.unshift(iconWnum);
-                    
-                    
+
+
                     output.append("<a href='#' class='day-container dayc"+ [i+1] +"'><div class='icon circle color" + iconWnum + "'></div><div class='date'><div class='week-day'>" + weekDay + "</div>" + " <div class='week-day-long'>" + weekDayLong + "</div></div>" + "<div class='day-temp'>"+"<div class='temp-max'>" + tempMax + "º</div>" + "<div class='temp-min'>" + tempMin + "º</div></div><div class='fun-content" + [i+1] + "'></div></a>");
-                    
+
                     $("#container .dayc1 .date .week-day").html("Today");
-                    
+
                     $(".fun-content"+[i+1]).append(txtFun[i]);
-                    
+
                 };
-                
-                
+
+
                 coiso();
-                
+
             });
 
         }
@@ -80,17 +80,16 @@ function geoFindMe() {
         }
 
         output.append("<p class='locating'>Locating…</p>");
-        
+
         navigator.geolocation.getCurrentPosition(success, error);
-        
+
     }
-    
+
 }
-    
 
-    geoFindMe();
+geoFindMe();
 
-
+    /* click to expand day-container */
     function coiso() {
         $(".day-container").click(function(){
 
@@ -103,14 +102,13 @@ function geoFindMe() {
                     }, 300 );
              }
             else {
-                $(this).addClass("active");
+                $(this).addClass("active m");
                 $(this).animate({
                     height: 200
                     }, 300 );
             }
 
-            $("#container .dayc1 .fun-content").html("This is where cool stuff appears!");
         });
     }
-    
+
 });
